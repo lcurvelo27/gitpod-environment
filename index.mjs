238aab1c5c55307fc4cb8de4dfb9a1fbf2ef5c64
Luis Curvelo <lcurvelo27@gmail.com>
2022-04-26 12:09:09 -0600

    add example

--- index.mjs
+++ index.mjs
@@ -1,3 +1,9 @@
+//write a function that takes a string representing a roman numerals and returns its value as an integer.
+// example input: "XXII"
+// expected output: 22
+
+// you can safely assume that all inputs will be valid roman numerals
+
 export function romanNumeralsTranslator() {
   return 1
 }
